acc4a1fc2f4edf6711f3550147f607c45d029800
Christina Massage <lokaltexte.service@gmail.com>
2026-04-11 22:33:17 +0200

    Fix booking page and build

--- app/booking/page.tsx
+++ app/booking/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useMemo, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { supabase } from "@/lib/supabase";
+import { supabase } from "../lib/supabase";
 
 type Language = "de" | "hu";
 
